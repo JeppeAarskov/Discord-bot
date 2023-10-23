4537a8c75785ca7b0418e6ec6979bace71a82cea
JeppeAarskov <jepperdebber@gmail.com>
2023-10-23 11:53:00 +0200

    initialize

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
+const { Client, Intents } = require('discord.js');
 
 const client = new Client({
     intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
+        Intents.FLAGS.Guilds,
+        Intents.FLAGS.GuildMembers,
+        Intents.FLAGS.GuildMessages,
+        Intents.FLAGS.MessageContent,
     ],
 });
 
-client.on('ready', (c) => {
+client.once('ready', () => {
     console.log('Asbjørns mor er klar til dig');
 });
 
